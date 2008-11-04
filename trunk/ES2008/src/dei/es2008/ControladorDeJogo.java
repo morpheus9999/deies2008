bb1dd47a16ad80ca2ade83793edcdd62394eca28
pascoal@student.dei.uc.pt <pascoal@student.dei.uc.pt@7195b9d8-a9c9-11dd-a77a-2d921c902ce3>
2008-11-04 23:22:27 +0000

    controlador basico

--- trunk/ES2008/src/dei/es2008/ControladorDeJogo.java
+++ trunk/ES2008/src/dei/es2008/ControladorDeJogo.java
@@ -28,33 +28,33 @@ public class ControladorDeJogo {
 
     public void ControladorDeJogo() {
         // TODO: implement
-        this.mundo=new Mundo();
     }
     
     /** @pdOid 8a29e47a-dd73-4a0d-a56f-9206850587c4 */
     
     public void iniciarJogo() {
-        // TODO: implement
+        this.mundo=new Mundo();
     }
 
     /** @pdOid c7ee8da6-ce9f-4a5d-ae80-c7b0adee3768 */
     public void sairJogo() {
-        // TODO: implement
+       System.exit(0);
     }
 
     /** @pdOid c5ff1938-3ac8-433f-8755-61a2a68c8d56 */
     public void verRanking() {
-        // TODO: implement
+      ficheiro.carregarRanking();
+      //assumindo que ele na classe ficheiros carrega e imprime
     }
 
     /** @pdOid ebe7fd04-e4b6-4bc0-94aa-63b452b25c22 */
     public void gravarJogo() {
-        // TODO: implement
+        ficheiro.gravarJogo(jogo);
     }
 
     /** @pdOid 26d35a9f-f9c4-4ec1-9ff5-2f939826e711 */
     public void carregarJogo() {
-        // TODO: implement
+        ficheiro.carregarJogo();
     }
 
     /** @pdOid af659177-3570-4a70-a061-13d418c57cc9 */
@@ -65,16 +65,18 @@ public class ControladorDeJogo {
     /** @param estado
      * @pdOid aa36fe23-641f-4d92-98dd-a68c2ccd9555 */
     public void actualizaEstadoPeca(int estado) {
-        // TODO: implement
+        
     }
 
     /** @pdOid 9a6201c4-bc46-487c-ba67-a4249f8ded95 */
     public void actualizaMundo() {
-        // TODO: implement
+        jogo.carregarInstante();
     }
 
     /** @pdOid f17cdf90-d33a-4800-9d47-7079d34a3a1d */
     public void pausarjogo() {
-        // TODO: implement
+        /*thread.setPaused(true);
+        board.setMessage("Paused");
+        component.button.setLabel("Resume");*/
     }
 }
